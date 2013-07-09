b2ec30da1673e43cd8f12b7b97b6db4ec8cd97e9
Pavana Teja T <teja1221@gmail.com>
2013-07-09 20:50:50 +0530

    fixed facebook code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,23 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
-    @user.facebook_user_details(params[:user][:access_token])
-    if @user.save
-      flash[:notice] = "User created successfully"
-      redirect_to user_path(@user)
+    if User.where(access_token: params[:user][:access_token]).length > 0
+      user = User.where(access_token: params[:user][:access_token]).last
+      redirect_to sample_user_path(user.username)
     else
-      flash[:error] = "User not created successfully"
-      render "new"
+      @user = User.new(params[:user])
+      @user.facebook_user_details(params[:user][:access_token])
+      if @user.save
+        flash[:notice] = "User created successfully"
+        redirect_to sample_user_path(@user.username)
+      else
+        flash[:error] = "User not created successfully"
+        render "new"
+      end
     end
+  end
+
+  def show
+    @user = User.find_by_username(params[:username])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   attr_accessible :bio, :gender, :hometown, :location, :name, :username, :access_token
-  validates_presence_of :access_token
 
   def facebook_user_details(access_token)
     graph = FacebookApi.graph(access_token)
+    profile_details = graph.get_object("me")
+    self.username = profile_details["username"]
+    self.location = profile_details["location"]["name"]
+    self.name = profile_details["name"]
+    self.gender = profile_details["gender"]
+    self.hometown = profile_details["hometown"]["name"]
+    self.bio = profile_details["bio"]
   end
 end
